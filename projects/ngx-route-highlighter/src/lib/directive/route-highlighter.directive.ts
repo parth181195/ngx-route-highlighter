4ea5dd6f55098e6eee50fae9d9b45fe74f385467
parth181195 <parthjansari@outlook.com>
2019-11-18 14:24:44 +0530

    removed conditions and added ragex for link match

--- projects/ngx-route-highlighter/src/lib/directive/route-highlighter.directive.ts
+++ projects/ngx-route-highlighter/src/lib/directive/route-highlighter.directive.ts
@@ -17,8 +17,6 @@ export class RouteHighlighterDirective implements OnChanges, OnDestroy {
     strict: false
   };
 
-  // TODO: (\/visitor\/[a-zA-Z0-9]+\/request\/[a-zA-Z0-9]+) use ragex for matching url instade of if elseg
-
   @Input() ngxRouteHighlighter: NgxRouteHighlighterConfig = new NgxRouteHighlighterConfig(this.defaultConfig);
   @Input() activeClass = 'active';
 
@@ -26,6 +24,7 @@ export class RouteHighlighterDirective implements OnChanges, OnDestroy {
     this.ngxRouteHighlighter = new NgxRouteHighlighterConfig(this.defaultConfig);
   }
   constructor(private el: ElementRef, private location: Location, private r2: Renderer2, private router: Router) {
+    console.clear();
     router.events.subscribe(event => {
       if (event instanceof NavigationStart) {
         this.r2.removeClass(this.el.nativeElement, this.activeClass);
@@ -48,41 +47,33 @@ export class RouteHighlighterDirective implements OnChanges, OnDestroy {
 
 
   setClass() {
-    // console.log(this.linkMatcher(this.ngxRouteHighlighter.config.links, this.ngxRouteHighlighter.config.strict))
     if (this.linkMatcher(this.ngxRouteHighlighter.config.links, this.ngxRouteHighlighter.config.strict)) {
       this.r2.addClass(this.el.nativeElement, this.activeClass);
     }
   }
 
   linkMatcher(links, strict?) {
-    let foundMatch = false;
-    let temp = {};
-
+    const foundMatch = [];
     if (!strict) {
       strict = false;
+    } else {
+      console.warn('strict is dapricated it will not have any effect as of now and it will be removed after version 0.1.0');
     }
     if (isString(links)) {
       links = [links];
     }
     if (links) {
-      links.forEach(link => {
-        console.log(`${this.location.path()} is in ${links} => ${this.location.path().includes(link) === link}`)
-        if (strict) {
-          if (this.location.path() === link) {
-            foundMatch = true;
-            temp[link] = foundMatch;
-          }
-        } else {
-          if (this.location.path().includes(link)) {
-            foundMatch = true;
-          }
-          // (\/test-2)?(\/trst)?
-        }
-        temp[link] = foundMatch;
+      links.forEach((link, index) => {
+        const $r: RegExp = this.regExpGen(link);
+        foundMatch.push($r.test(this.location.path()));
       });
     }
-    console.log(temp)
-    return foundMatch;
+    console.log(foundMatch);
+    return foundMatch.indexOf(true) !== -1;
+  }
+  regExpGen(link) {
+    const ragexBaseString = link.replace(new RegExp(/(\/\*)/, 'gmi'), '\/[a-z0-9]+');
+    return new RegExp(String.raw`(${ragexBaseString})`, 'gmi');
   }
 }
 
